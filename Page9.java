483524357e99826dba81d252961b5824675fc13a
Hacker091356 <ninno2812@gmail.com>
2024-02-07 14:45:16 -0500

    ll

---
+++ Page9.java
@@ -0,0 +1,15 @@
+
+/****
+Programmer: Neil Oza
+Date: February, 7. 2023
+Program Name: Page9.java
+*************/
+
+public class Page9
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hi"); //message to user
+        System.out.println("Page9 "); //assignment
+    }
+}
